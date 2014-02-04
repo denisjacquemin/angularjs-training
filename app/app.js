c83f24e9a655e0f91228bdf397b4252865b6dd87
davidb583 <fishing_junk@yahoo.fr>
2014-02-04 17:19:10 +0100

    exercice 4a1

--- app/app.js
+++ app/app.js
@@ -10,6 +10,9 @@ myApp.config(function ($routeProvider) {
             templateUrl: 'app/exo2/exo2.html'
         }).when('/error', {
             templateUrl: 'app/error/error.html'
+        }).when('/exo4', {
+            templateUrl: 'app/exo4/exo4.html',
+            controller: 'Exo4Ctrl'
         }).otherwise({
             redirectTo: '/error'
         });
---
+++ app/exo4/exo4Ctrl.js
@@ -0,0 +1,7 @@
+/* global myApp */
+/* global console */
+"use strict";
+
+myApp.controller('Exo4Ctrl', function ($scope) {
+    $scope.doesItWorks = "Yes it works";
+});
