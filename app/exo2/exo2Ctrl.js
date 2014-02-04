c90db17f3ad09d540fbde8419fe01209ef364aa3
davidb583 <fishing_junk@yahoo.fr>
2014-02-04 16:40:57 +0100

    added a nested databinding

--- app/exo2/exo2Ctrl.js
+++ app/exo2/exo2Ctrl.js
@@ -1,3 +1,10 @@
 myApp.controller('Exo2Ctrl', function($scope) {
     $scope.displayingFromCtrl = "Hello, world from controller!";
+
+    $scope.displayHouseNumber = function() {
+        console.log($scope.user);
+        console.log($scope.user.house);
+        console.log($scope.user.house.number);
+    };
+
 });
