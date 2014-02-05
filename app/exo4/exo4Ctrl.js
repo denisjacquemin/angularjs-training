fa1895262cff7ddfb43ce101743ca3730d4b91cb
davidb583 <fishing_junk@yahoo.fr>
2014-02-05 16:49:08 +0100

    reset workspace

--- app/exo4/exo4Ctrl.js
+++
@@ -1,9 +0,0 @@
-/* global myApp */
-/* global console */
-"use strict";
-
-myApp.controller('Exo4Ctrl', function ($scope, HttpSrvc, loadedDatas) {
-    $scope.doesItWorks = "Yes it works";
-    //$scope.datas = HttpSrvc.getData();
-    $scope.datas = loadedDatas;
-});
--- app/exo4/underscoreFcty.js
+++
@@ -1,8 +0,0 @@
-/* global myApp */
-
-"use strict";
-
-// naming convention is not followed here, "u" is more convenient.
-myApp.factory('u', function ($window) {
-    return $window._; // assumes underscore has already been loaded on the page
-});
