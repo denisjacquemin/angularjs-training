d43aaf7cdb4dc9b13ccb7ec1610d184f6c348677
davidb583 <fishing_junk@yahoo.fr>
2014-02-05 10:06:50 +0100

    exercise 4c and 4d

--- app/exo4/exo4Ctrl.js
+++ app/exo4/exo4Ctrl.js
@@ -6,5 +6,10 @@ myApp.controller('Exo4Ctrl', function ($scope, HttpSrvc) {
     $scope.doesItWorks = "Yes it works";
 //    console.log("HttpSrvc is ");
 //    console.log(HttpSrvc);
-    $scope.datas = HttpSrvc.getData();
+    //$scope.datas = HttpSrvc.getData();
+    HttpSrvc.wrappedGet("/rest/quote").then(
+        function successCallback(quotes) {
+            $scope.datas = quotes;
+        }
+    );
 });
--- app/exo4/httpSrvc.js
+++ app/exo4/httpSrvc.js
@@ -2,10 +2,25 @@
 /* global console */
 "use strict";
 
-myApp.service('HttpSrvc', function ($http) {
+myApp.service('HttpSrvc', function ($http, $q) {
     return {
         getData: function () {
             return [1, 2, 3];
+        },
+        wrappedGet: function (url, config) {
+            var deferred = $q.defer();
+            $http.get(url, config).then(
+
+                function successCallback(result) {
+                    deferred.resolve(result.data);
+                },
+
+                function errorCallback(result) {
+                    deferred.reject(result);
+                }
+
+            );
+            return deferred.promise;
         }
     };
 });
